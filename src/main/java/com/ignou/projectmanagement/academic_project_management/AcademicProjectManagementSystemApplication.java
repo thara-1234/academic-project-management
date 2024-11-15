984f8c1c9da3fd0c50ae44468ef5a0033c080850
thara-1234 <tharakrishna1989@gmail.com>
2024-11-15 23:02:34 +0530

    initial project commit

---
+++ src/main/java/com/ignou/projectmanagement/academic_project_management/AcademicProjectManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ignou.projectmanagement.academic_project_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicProjectManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicProjectManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ignou/projectmanagement/academic_project_management/AcademicProjectManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ignou.projectmanagement.academic_project_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicProjectManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
